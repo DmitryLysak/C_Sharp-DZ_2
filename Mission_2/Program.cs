114d08df5dd346bb64b9085f185a0ae01d2137f2
Dmitry Lysak <lysak.d@gmail.com>
2023-02-13 10:54:24 +0300

    added complit Mission_2

--- Mission_2/Program.cs
+++ Mission_2/Program.cs
@@ -1,10 +1,11 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
 // или сообщает, что третьей цифры нет.
+
 Console.WriteLine("Введите число");
 string input = Console.ReadLine();
 int limit = 2;
 if (input.Length <= limit)
-Console.WriteLine("Третьей цифры нет");
+    Console.WriteLine("Третьей цифры нет");
 else
-Console.WriteLine("Третья цифра этого числа " + input[2]);
+    Console.WriteLine("Третья цифра этого числа " + input[2]);
 
